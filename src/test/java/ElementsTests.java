782701ce2e2b966b6974f8f3c4327a4dc6727ddc
zuram2006 <zuram@bit.ge>
2023-05-31 15:26:48 +0400

    initial commmit

--- src/test/java/ElementsTests.java
+++ src/test/java/ElementsTests.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class ElementsTests {
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.Test;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.testng.Assert;
+
+
+public class ElementsTests {
+    @Test
+    public void testRadioButtonSelection() {
+        Selenide.open("https://demoqa.com/");
+        Selenide.open("https://demoqa.com/");
+
+
+        Selenide.$x("//span[text()='Elements']").click();
+        Selenide.$x("//span[text()='Radio Button']").click();
+        Selenide.$x("//label[text()='Impressive']").click();
+        Assert.assertTrue(Selenide.$x("//p[text()='You have selected Impressive']").exists());
+
+    }
+
+    @Test(dependsOnMethods = "testRadioButtonSelection")
+    public void dependentTestMethod() {
+
+
+        Selenide.open("https://demoqa.com/");
+        Selenide.$x("//span[text()='Elements']").click();
+        Selenide.$x("//span[text()='Buttons']").click();
+        Selenide.$("#dynamicClickMessage").click();
+        Assert.assertTrue(Selenide.$("#dynamicClickMessage").shouldHave(Condition.text("You have done a dynamic click")).exists());
+        Selenide.actions().contextClick(Selenide.$("#rightClickBtn")).perform();
+        Assert.assertTrue(Selenide.$("#rightClickMessage").shouldHave(Condition.text("You have done a right click")).exists());
+    }
 }
